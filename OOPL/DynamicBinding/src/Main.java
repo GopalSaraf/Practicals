1c7f83c61ab2b534fcce7378279a5ceb1e656378
GopalSaraf <sarafgopal37@gmail.com>
2022-09-21 14:40:39 +0530

    Dynamic Binding

---
+++ OOPL/DynamicBinding/src/Main.java
@@ -0,0 +1,51 @@
+// Title : Dynamic Binding
+// Problem Statement :
+// Design a base class shape with two double type values and member functions
+// to input the data and compute_area() for calculating area of shape. Derive two classes’
+// triangle and rectangle. Make compute_area() as abstract function and redefine this
+// function in the derived class to suit their requirements. Write a program that accepts
+// dimensions of triangle/rectangle and display calculated area. Implement dynamic
+// binding for given case study.
+//
+// Program by : 23168 Gopal Saraf
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char option;
+
+        while (true) {
+            System.out.println();
+            System.out.println("Choose an option");              // Menu
+            System.out.println("a - Compute area of triangle");
+            System.out.println("b - Compute area of rectangle");
+            System.out.println("c - Quit program");
+            System.out.println();
+            System.out.print("Your Option > ");
+            option = sc.next().charAt(0);
+            System.out.println();
+
+            switch (option) {
+                case 'a' -> {
+                    Triangle triangle = new Triangle();     // Triangle object
+                    triangle.takeDimensions();              // Taking dimensions
+                    System.out.println();
+                    triangle.computeArea();                 // Computing area
+                }
+                case 'b' -> {
+                    Rectangle rectangle = new Rectangle();  // Rectangle object
+                    rectangle.takeDimensions();             // Taking dimensions
+                    System.out.println();
+                    rectangle.computeArea();                // Computing area
+                }
+                case 'c' -> {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Incorrect Option. Try again...");
+            }
+        }
+    }
+}
---
+++ OOPL/DynamicBinding/src/Shape.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+abstract class Shape {
+    protected int length;   // length of shape
+    protected int breadth;  // breadth of shape
+    protected Scanner sc = new Scanner(System.in);
+
+    Shape(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    Shape() {
+        this(0, 0);
+    }
+
+    protected void setLength(int length) {
+        this.length = length;
+    }
+
+    protected void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+
+    abstract public void computeArea();
+}
